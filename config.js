6d3165ec64780c547993d73726039393eae310f3
AstrantV <AstrantV.creator@gmail.com>
2023-01-10 19:34:00 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,54 @@
+module.exports = {
+    config: 
+    {
+        'messages': {
+            'leaderboard': '1030404879585398794',
+            'data': '1030391553195266049'
+        },
+
+        'channels': {
+            'plumes': '1028004339974938678',
+            'leaderboard': '1028714070150500475',
+            'welcome': '1028004339974938680',
+            'presentation': '1028004339974938680',
+            'text': '1028004340490829959',
+            'textForum': '1035443655932129300',
+            'general': '1028004340490829962',
+            'sprint': '1028004340763459628',
+            'cya': '1028004340960612432',
+            'delete':'1030530341334692001',
+            'logs': '1030360521586782238',
+            'tournamentRules': '1056835990519550012',
+            'monthlyNovel': '1056835898689466428',
+            'nologs': ['💾scriptu-logs','👀panoptique-del'],
+            'safe': '1059939069389443072'
+        },
+
+        'plumesRoles': {
+            '1028004339194810420': 1,
+            '1028004339194810421': 25,
+            '1028004339194810422': 50,
+            '1028004339194810423': 100,
+            '1028004339194810424': 200,
+            '1028004339194810425': 666666666666666666
+        },
+
+        'messageReplies': {
+            'CHAD': 'https://tenor.com/view/mujikcboro-seriymujik-gif-24361533',
+            ' rick': 'https://tenor.com/view/rickroll-rick-roll-gif-gif-25399889',
+            'nuck': 'https://i.imgur.com/N3oxDNW.gifv',
+            'RANK': 'https://i.imgur.com/hK9qUWc.gifv'
+        },
+
+        'emotes': {
+            'plume':'<:Scriptuplume:1027094890099781673>'
+        },
+
+        'roles': {
+            'sprinter':'1028004339194810424',
+            'tournament':'1056834227783606283'
+        }
+
+    }
+
+}

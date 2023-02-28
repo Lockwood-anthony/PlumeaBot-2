69d7cad2d03bfdd337c218f5bbd06aa11955a1f0
AstrantV <AstrantV.creator@gmail.com>
2023-02-28 16:45:01 +0100

    a

--- config.js
+++ config.js
@@ -1,159 +1,169 @@
 module.exports = {
-    config: 
-    {
-        messages: {
-            leaderboard: '1080134170124222587',
-            tutoPost: 'https://discord.com/channels/1066783578140180520/1066783579079716895/1075837538595520655',
-            tutoComment: 'https://discord.com/channels/1066783578140180520/1066783579079716895/1075837538595520655'
+    config:
+        {
+            messages: {
+                leaderboard: '1060999836024963194',
+                tutoPost: 'https://discord.com/channels/1066783578140180520/1066783579079716895/1075837538595520655',
+                tutoComment: 'https://discord.com/channels/1066783578140180520/1066783579079716895/1075837538595520655'
 
-        },
+            },
 
-        channels: {
-            sesame: '1080133619043020860',
-            plumes: '1080133619043020864',
-            leaderboard: '1080133619043020865',
-            counter: '1080133619043020861',
-            welcome: '1080133619043020868',
-            presentation: '1080133619043020869',
-            ticket: '1080133619043020866',
-            textForum: '1080134765904150648',
-            textRequest: '1080133619361775670',
-            text: '1080133621161136186',
-            central: '1080133619361775669',
-            monthlyNovel: '1080133619361775675',
-            general: '1080133619831541804',
-            sprint: '1080133619831541801',
-            cya: '1080133620691370059',
-            delete:'1080133621161136184',
-            logs: '1080133621161136187',
-            safe: '1080133621161136185'
-        },
+            channels: {
+                sesame: '1070792810334261309',
+                plumes: '1060681628411777054',
+                leaderboard: '1060681664352751666',
+                counter: '1076180642443833376',
+                welcome: '1066783578698027185',
+                presentation: '1060682713960562768',
+                ticket: '1060681924877766786',
+                textForum: '1076842352397791243',
+                textRequest: '1079661344556580915',
+                text: '1079661496742719488',
+                central: '1079710593239486524',
+                monthlyNovel: '1060679258579669083',
+                general: '1060677819107115088',
+                sprint: '1060682429565767760',
+                cya: '1060682282874187866',
+                delete:'1060682339451158599',
+                logs: '1060680852729770166',
+                safe: '1060680793241960458'
+            },
 
-        plumesRoles: {
-            '1080133618279645273': {
-                p: 1,
-                color: 'a9ffd1',
-                mes: " Vous pouvez poster gifs, fichiers, et liens, et bien évidemment votre texte dans le salon prévu. Vous pouvez parler dans un salon vocal."
+            plumesRoles: {
+                '1027119828504424478': {
+                    p: 1,
+                    color: 'a9ffd1',
+                    mes: " Vous pouvez poster gifs, fichiers, et liens, et bien évidemment votre texte dans le salon prévu. Vous pouvez parler dans un salon vocal."
+                },
+                '1027119923509612597': {
+                    p: 25,
+                    color: '97e8ff',
+                    mes: " Vous pouvez faire votre publicité, mettre en avant vos réseaux ou un projet vous tenant à cœur dans le salon prévu."
+                },
+                '1027120011459952680': {
+                    p: 50,
+                    color: 'ff9494',
+                    mes: " Contactez le staff via ticket et décidez de la prochaine emote à adopter."
+                },
+                '1027120127768010783': {
+                    p: 100,
+                    color: '00ff74',
+                    mes: "Profitez d'un salon VIP exclusif : Le Palais. \n" +
+                        "Vous pouvez aussi désormais faire partie du jury des Pluméades."
+                },
+                '1027120189445259284': {
+                    p: 200,
+                    color: '00b2ff',
+                    mes: "Contactez le staff via ticket et décidez d'un rôle permanent et d'une couleur exclusive."
+                },
+                '1075353931997253642': {
+                    p: 500,
+                    color: 'ff0000',
+                    mes: "Tu es un véritable dieu à présent, nous n'avons plus rien à t'offrir que ce tu n'aies déjà. \n" +
+                        "Tu as gagné notre éternel respect.\n" +
+                        "Merci infiniment pour ton dévouement"
+                },
+                '1027850518321692692': {
+                    p: 666666666666666666,
+                    color: '080202',
+                    mes: "lol"
+                }
             },
-            '1080133618279645274': {
-                p: 25,
-                color: '97e8ff',
-                mes: " Vous pouvez faire votre publicité, mettre en avant vos réseaux ou un projet vous tenant à cœur dans le salon prévu."
-            },
-            '1080133618321592440': {
-                p: 50,
-                color: 'ff9494',
-                mes: " Contactez le staff via ticket et décidez de la prochaine emote à adopter."
-            },
-            '1080133618321592441': {
-                p: 100,
-                color: '00ff74',
-                mes: "Profitez d'un salon VIP exclusif : Le Palais. \n" +
-                    "Vous pouvez aussi désormais faire partie du jury des Pluméades."
-            },
-            '1080133618321592442': {
-                p: 200,
-                color: '00b2ff',
-                mes: "Contactez le staff via ticket et décidez d'un rôle permanent et d'une couleur exclusive."
-            },
-            '1080133618321592443': {
-                p: 500,
-                color: 'ff0000',
-                mes: "Tu es un véritable dieu à présent, nous n'avons plus rien à t'offrir que ce tu n'aies déjà. \n" +
-                    "Tu as gagné notre éternel respect.\n" +
-                    "Merci infiniment pour ton dévouement"
-            },
-            '1080133618321592444': {
-                p: 666666666666666666,
-                color: '080202',
-                mes: "lol"
-            }
-        },
 
-        messageReplies: {
-            CHAD: 'https://tenor.com/view/mujikcboro-seriymujik-gif-24361533',
-            ' rick': 'https://tenor.com/view/rickroll-rick-roll-gif-gif-25399889',
-            nuck: 'https://i.imgur.com/N3oxDNW.gifv',
-            RANK: 'https://i.imgur.com/hK9qUWc.gifv'
-        },
-
-        emotes: {
-            plume: '❤️',
-            love: '❤️'
-        },
-
-        roles: {
-            staff: '1080133618321592448',
-            guard: '1080133618321592446',
-            plumeen: '1080133618241908808',
-            dev: '1080133618321592447'
-        },
-
-        string: {
-            serverName: 'pluméa',
-            inhab: 'pluméen'
-
-        },
-
-        tutoIds: {
-            commentaire: 1
-
-        },
-
-        themes: [
-            {
-                name: 'Science Fiction',
-                desc: 'Tu aimes les aventures spatiales, les technologies futuristes et les récits avec des aliens?',
-                id: '1080135772671311882'
+            messageReplies: {
+                CHAD: 'https://tenor.com/view/mujikcboro-seriymujik-gif-24361533',
+                ' rick': 'https://tenor.com/view/rickroll-rick-roll-gif-gif-25399889',
+                nuck: 'https://i.imgur.com/N3oxDNW.gifv',
+                RANK: 'https://i.imgur.com/hK9qUWc.gifv'
             },
-            {
-                name: 'Fantasy ',
-                desc: 'Plonge dans les histoires de mondes imaginaires, créatures mystiques et autres épopées magiques',
-                id: '1080136122161696819'
-            },
-            {
-                name: 'Historique',
-                desc: 'Toujours rêvé de vivre une époque révolue de notre histoire? le moment est venu.',
-                id: '1080136167393067129'
-            },
-            {
-                name: 'Jeunesse',
-                desc: 'La voie est longue pour devenir adulte, certaines péripéties valent bien être contées.',
-                id: '1080136194614120560'
-            },
-            {
-                name: 'Polar',
-                desc: 'Intéressé par un roman policier soupoudré de critique sociale?',
-                id: '1080136231951802408'
-            },
-            {
-                name: 'Action',
-                desc: 'Deux mots, adrénaline et aventure !',
-                id: '1080136255918051390'
-            },
-            {
-                name: 'Drame',
-                desc: 'La fatalité semble tous nous avoir à sa merci, et pourtant, certains se lèvent pour la défier',
-                id: '1080136279649431567'
-            },
-            {
-                name: 'Comédie',
-                desc: 'Besoin de lecture sans prise de tête et joviale?',
-                id: '1080136303884120084'
-            },
-            {
-                name: 'Averti',
-                desc: 'Contient du contenu explicit, déconseillé aux lecteurs mineurs ou sensibles (violence, sexe...)',
-                id: '1080136329762967652'
-            },
-            {
-                name: 'Autre',
-                desc: 'Pour les textes uniques en leur genre',
-                id: '1080136347144171584'
-            }
 
-        ]
+            emotes: {
+                plume: '<:Plumea:1062260412600557598>',
+                love: '<:Midori:1062488633594101831>'
+            },
 
-    }
+            roles: {
+                staff: '1027119511054327858',
+                guard: '1056996103213563914',
+                plumeen: '1070790483879677962',
+                dev: '1060878507242160260'
+            },
+
+            string: {
+                serverName: 'pluméa',
+                inhab: 'pluméen'
+
+            },
+
+            tutoIds: {
+                commentaire: 1
+
+            },
+
+            themes: [
+                {
+                    name: 'Science Fiction',
+                    desc: 'Tu aimes les aventures spatiales, les technologies futuristes et les récits avec des aliens?',
+                    id: '1076843360339378277'
+                },
+                {
+                    name: 'Fantasy ',
+                    desc: 'Plonge dans les histoires de mondes imaginaires, créatures mystiques et autres épopées magiques',
+                    id: '1076843379536703518'
+                },
+                {
+                    name: 'Horreur',
+                    desc: 'Une seule règle, ne jamais lire dans le noir (ça abîme la rétine)',
+                    id: '1076843422557667381'
+                },
+                {
+                    name: 'Historique',
+                    desc: 'Toujours rêvé de vivre une époque révolue de notre histoire? le moment est venu.',
+                    id: '1076843400839569408'
+                },
+                {
+                    name: 'Jeunesse',
+                    desc: 'La voie est longue pour devenir adulte, certaines péripéties valent bien être contées.',
+                    id: '1076843442522554388'
+                },
+                {
+                    name: 'Polar',
+                    desc: 'Intéressé par un roman policier soupoudré de critique sociale?',
+                    id: '1076843503973318726'
+                },
+                {
+                    name: 'Romance',
+                    desc: 'Un peu de douceur et d~émotions',
+                    id: '1076843524902891580'
+                },
+                {
+                    name: 'Action',
+                    desc: 'Deux mots, adrénaline et aventure !',
+                    id: '1076843542196015104'
+                },
+                {
+                    name: 'Drame',
+                    desc: 'La fatalité semble tous nous avoir à sa merci, et pourtant, certains se lèvent pour la défier',
+                    id: '1076843603915190422'
+                },
+                {
+                    name: 'Comédie',
+                    desc: 'Besoin de lecture sans prise de tête et joviale?',
+                    id: '1076843587439964290'
+                },
+                {
+                    name: 'Averti',
+                    desc: 'Contient du contenu explicit, déconseillé aux lecteurs mineurs ou sensibles (violence, sexe...)',
+                    id: '1076844601962414193'
+                },
+                {
+                    name: 'Autre',
+                    desc: 'Pour les textes uniques en leur genre',
+                    id: '1077261859926847520'
+                }
+
+            ]
+
+        }
 
 }

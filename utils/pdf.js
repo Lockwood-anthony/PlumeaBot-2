2977b3d9704b44db73b11be419e0145a850a5888
AstrantV <AstrantV.creator@gmail.com>
2023-11-23 12:54:30 +0100

    a

--- utils/pdf.js
+++ utils/pdf.js
@@ -12,7 +12,7 @@ module.exports = {
             .pipe(fs.createWriteStream(file_path))
 
         console.log(";3")
-        file = fs.readFileSync(file_path)
+        let file = fs.readFileSync("../config")
         let data = await pdf(file)
         console.log(";3")
 

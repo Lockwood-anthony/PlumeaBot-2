e9dabfbfa1cb6a6566da4e407e7e3898153761be
AstrantV <AstrantV.creator@gmail.com>
2023-11-23 11:10:26 +0100

    a

--- utils/pdf.js
+++ utils/pdf.js
@@ -4,16 +4,37 @@ const path = require("path");
 module.exports = {
 
     async countWords(file){
-        console.log(file.url)
-        let data = await pdf(file.url)
+
+        fetch(file.url)
+            .then(response => response.blob())
+            .then(blob => {
+                const url = URL.createObjectURL(blob)
+                const a = document.createElement('a')
+                a.href = url
+                a.download = file.url + '.pdf'
+                a.click()
+                URL.revokeObjectURL(url)
+            })
+        .catch(error => {
+            console.error('File download failed:', error)
+        })
         console.log(";3")
+
+        let data = await pdf('file.pdf')
+
         data = data.text
-        console.log(";3")
         data = data.replace(/(^\s*)|(\s*$)/gi,"")
         data = data.replace(/[ ]{2,}/gi," ")
         data = data.replace(/\n /,"\n")
-        console.log(";3")
-        return data.split(" ").length
+        
+        l = data.split(" ").length
+
+        unlink(file.url + '.pdf', (err) => {
+            if (err) throw err
+            console.log(file.url + '.pdf was deleted')
+        })
+
+        return l
 
     },
 

5922f2da615197ce7dae9f6a5b854ddfd5820071
AstrantV <AstrantV.creator@gmail.com>
2023-11-23 11:33:09 +0100

    a

--- utils/pdf.js
+++ utils/pdf.js
@@ -8,7 +8,7 @@ module.exports = {
     async countWords(file){
         file_path = "~/" + file.url + '.pdf'
 
-        request.get(file.url)
+        await request.get(file.url)
             .on('error', console.error)
             .pipe(fs.createWriteStream(file_path))
         console.log(";3")

2debed6fdaaeea793b55cc72b622145dd60a7813
AstrantV <AstrantV.creator@gmail.com>
2023-11-28 22:29:23 +0100

    a

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -19,7 +19,6 @@ module.exports = {
 
             for await (t of all_texts) {
                 console.log(t)
-                console.log(t.postId)
 
                 if(t.postId ==0 ) break
                 let post = await client.channels.fetch(t.postId)

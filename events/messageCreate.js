0bc77ff44048af6945a173f6c7e497282d0bfe2f
AstrantV <AstrantV.creator@gmail.com>
2023-11-28 19:18:26 +0100

    a

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -20,9 +20,7 @@ module.exports = {
 
             threads = await threads.threads
             for await (t of threads) {
-                t = await client.channels.fetch(t.id)
-                let name = await t.name
-                console.log(name)
+                let name = await t[1].name
 
                 name = name.split(" ")
                 id = name[0]
